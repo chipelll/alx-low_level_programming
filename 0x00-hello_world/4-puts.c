4abcb2c9fd3da3a82791ab8057df5221fd84902d
chipelll <chipel009@yahoo.com>
2022-09-08 15:09:44 -0700

    Betty Error

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) /*This is the entry point of the program */
 {
 	char Program[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Program);
 
-	return 0;
+	return (0);
 }
