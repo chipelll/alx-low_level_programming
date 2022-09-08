962fe24dc1d5443ca3527a121e02b31f71cfaf5a
chipelll <chipel009@yahoo.com>
2022-09-08 15:37:21 -0700

    Betty Error.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 /**
- * Main function serves as a starter
+ * Put - Prints out a statement
  * Void: This means nothing is available to fill up the space
  *
  * The main function serves as a pointer for the program to
  * know where to start coding from
- * Puts: This helps to print out a statement
  * Return: 0 if the program is an error
  */
-int main(void)
+int main(void) /*Main function is used as a program starter identifier*/
 {
 	char Program[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(Program);
+
 	return (0);
 }
