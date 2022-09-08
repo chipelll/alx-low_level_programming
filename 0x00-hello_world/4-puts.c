0687861599f4e223baab29e13533fd62ef02225c
chipelll <chipel009@yahoo.com>
2022-09-08 15:18:08 -0700

    Betty Error Fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(void) /*This is the entry point of the program */
+/**
+ *
+ *  * main; This signifies the entry point of the program
+ *
+ *   * void ; nothing
+ *     * Return: Value is zero which means success     
+ */
+int main(void)
 {
 	char Program[] = "\"Programming is like building a multilingual puzzle";
 
