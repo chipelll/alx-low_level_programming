8ed15636e3428e8e7ed5d66ff93f6e266d00c485
chipelll <chipel009@yahoo.com>
2022-09-10 14:23:58 -0700

    Putchar function to print number

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+*
+*Return: 0 if program is a success
+*/
+int main(void)
+{
+int ch;
+for (ch = 0; ch <= 9; ch++)
+{
+	putchar(ch%10 + '0');
+}
+putchar('\n');
+return (0);
+}
