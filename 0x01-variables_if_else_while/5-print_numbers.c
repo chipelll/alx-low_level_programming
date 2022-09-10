e5e6281b60e2d8b308b00c470aee26195769fb80
chipelll <chipel009@yahoo.com>
2022-09-10 13:58:22 -0700

    Decimal of single digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 if  program is a success
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+	printf("%d", x);
+}
+printf("\n");
+return (0);
+}
+
