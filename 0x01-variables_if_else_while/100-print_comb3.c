84b647d70748b80efee3c38933f0cd96b03a25f0
chipelll <chipel009@yahoo.com>
2022-09-11 12:15:59 -0700

    Combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 if program is a success
+ */
+int main(void)
+{
+int i;
+int r;
+for (i = 0; i <=8; i++)
+{
+	for (r = i + 1; r <= 9; r++)
+	{
+		if (r > i && r != i)
+		{
+			putchar(i + '0');
+			putchar(r + '0');
+		}
+		if ((i == 8) && (r == 9))
+		{
+			continue;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
