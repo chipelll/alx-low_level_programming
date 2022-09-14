974b16cd3218103bd37e59e761586d4fb3064e2d
chipelll <chipel009@yahoo.com>
2022-09-13 23:37:35 -0700

    Lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in lowercase and entry point
+ *
+ * Return: 0 if program is a success
+ */
+int main(void)
+{
+void print_alphabet(void);
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 #include <ctype.h>
 /*Function to print alphabets in lowercases using putchar()*/
 int _putchar(int c);
+void print_alphabet(void);
