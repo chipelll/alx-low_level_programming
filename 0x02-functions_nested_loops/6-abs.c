c60c53b822f017224eef8effc9d33c49f0344d70
chipelll <chipel009@yahoo.com>
2022-09-14 15:25:18 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - To get absolute value
+ * @x: function argument
+ *
+ * Return: 0 if success
+ */
+int _abs(int x)
+{
+if (x >= 0)
+{
+	return (x);
+}
+return (x * -1);
+}
