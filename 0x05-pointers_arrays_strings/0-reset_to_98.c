5a4507c5bed11e13d4c4e345d547e0acf6720778
chipelll <chipel009@yahoo.com>
2022-09-21 12:18:19 -0700

    Reset number value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets value to 98
+ * @n: function argument
+ *
+ * Return: 0 if program is a success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This file is contained in a program which deals with arrays and pointers.
