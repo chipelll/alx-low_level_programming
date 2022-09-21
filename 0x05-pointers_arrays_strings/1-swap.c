dca4b12b4280eba8c7b7e57b6c0e70caf793be81
chipelll <chipel009@yahoo.com>
2022-09-21 15:26:45 -0700

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - To swap integers
+ * @a: function argument
+ * @b: function argument
+ *
+ * Return: 0 if program runs seccessfully
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	int d;
+
+	c = *a;
+	d = *b;
+	*a = d;
+	*b = c;
+
+}
