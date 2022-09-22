a9a76f9555961dbdc9197d04626cdfc4a4b40200
chipelll <chipel009@yahoo.com>
2022-09-21 23:36:51 -0700

    Length of strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the lenght of a string@s: string pointer
+ * @s: string pointer
+ *
+ * Return: the lenght
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	for ( ; s[i] != '\0' ; i++)
+	{
+	}
+	return (i);
+}
