51677ceb851c837b43f707b51e9641b5472fb631
chipelll <chipel009@yahoo.com>
2022-09-22 15:19:42 -0700

    Some characters printed

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: string pointer
+ *
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len -= 1;
+
+	for (; i <= len ; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
