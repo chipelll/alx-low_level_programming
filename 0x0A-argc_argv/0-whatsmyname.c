cafa856e65362ce880af7164cb46c6db0f29fb99
chipelll <chipel009@yahoo.com>
2022-10-03 15:57:28 -0700

    Print executable file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name
+ * @argc - argument count
+ * @argv - argument list
+ *
+ * Return: 1 if success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[--argc]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This file helps understand the amount and type of command lin arguments used in running the program.
