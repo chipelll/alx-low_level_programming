5ad36e10e18f4bdcdb0bf7a142f9bd1019197ea1
chipelll <chipel009@yahoo.com>
2022-11-01 16:01:34 -0700

    Bits of mistakes corrected

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -22,16 +22,13 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	revnum = count = num = 0;
 
 	oldn = *n;
-
 	if (index > 63)
 
 		return (-1);
 
 	while (count < 63)
 	{
-
 		revnum = revnum << 1;
-
 		if (count == index)
 
 			revnum += 0;
@@ -41,27 +38,15 @@ int clear_bit(unsigned long int *n, unsigned int index)
 			revnum += oldn & 1;
 
 		count++;
-
 		oldn = oldn >> 1;
-
 	}
-
 	while (count > 0)
-
 	{
-
 		num = num << 1;
-
 		num += revnum & 1;
-
 		revnum = revnum >> 1;
-
 		count--;
-
 	}
-
 	*n = num;
-
 	return (1);
-
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,87 +10,45 @@
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
-
 {
-
 	unsigned long int nlen, mlen, flip, storen, storem;
 
-
-
 	nlen = mlen = flip = 0;
-
 	storen = n;
-
 	storem = m;
-
 	while (n > 0)
-
 	{
-
 		n = n >> 1;
-
 		nlen++;
-
 	}
-
 	while (m > 0)
-
 	{
-
 		m = m >> 1;
-
 		mlen++;
-
 	}
-
 	m = storem;
-
 	n = storen;
-
 	if (mlen > nlen)
-
 	{
-
 		while (mlen > 0)
-
 		{
-
 			if ((m & 1) != (n & 1))
-
 				flip += 1;
-
 			m = m >> 1;
-
 			n = n >> 1;
-
 			mlen--;
-
 		}
-
 	}
-
 	else
-
 	{
-
 		while (nlen > 0)
-
 		{
-
 			if ((m & 1) != (n & 1))
-
 				flip += 1;
-
 			m = m >> 1;
-
 			n = n >> 1;
-
 			nlen--;
-
 		}
-
 	}
-
 	return (flip);
-
 }
